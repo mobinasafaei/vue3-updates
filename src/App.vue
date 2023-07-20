74273e9c0a943eb8686fb3b6607d48da8fffa31f
unknown <safaeimobina1381@gmail.com>
2023-07-20 13:28:42 +0330

    add reactivity transform module

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div>
-    <h2>{{ msg }}</h2>
+    <!-- <h2>{{ msg }}</h2> -->
     <h3 @click="increament()">{{ count }}</h3>
-    <HomeView />
+    <!-- <HomeView /> -->
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
-import HomeView from "./components/HomeView.vue";
+// import HomeView from "./components/HomeView.vue";
 
-console.log("start");
-const msg = "this is mobina";
-const count = ref(0);
+// const msg = "this is mobina";
+const count = $ref(0)
 function increament() {
-  count.value++;
+  count++;
 }
 </script>
