6c5e8f4bad653a06fd606ff5f0ce02e8926ecf28
unknown <safaeimobina1381@gmail.com>
2023-07-20 17:29:43 +0330

    create web component

---
+++ src/components/CounterView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <button @click="count++">Increament</button>
+    <h2>{{ count }}</h2>
+    <button @click="count--">decrement</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const count = ref(0);
+</script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+// import { createApp } from 'vue'
+// import App from './App.vue'
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+
+import { defineCustomElement } from 'vue'
+import CounterView from "./components/CounterView.vue"
+
+const element = defineCustomElement(CounterView);
+
+customElements.define('my-counter',element);
