8a922961eb08b883fee56490aa275bf39840be14
unknown <safaeimobina1381@gmail.com>
2023-07-20 15:51:11 +0330

    convert  to ref

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,27 @@
 <template>
-  <div>{{ count2 }}</div>
+  <div>
+    <p>{{ number }}</p>
+    <button @click="number++"></button>
+  </div>
 </template>
 
 <script setup>
 import { $ref } from "vue/macros";
-import { ref } from "vue";
+import { $$ } from "vue/macros";
+import { ref, watch } from "vue";
 // import HomeView from "./components/HomeView.vue";
 
 // const msg = "this is mobina";
 const count1 = ref(0);
 let count2 = $ref(0);
 
+let number = $ref(0);
+trackChange($$(number));
+
+function trackChange(x) {
+  watch(x, () => {
+    console.log("x changes");
+  });
+}
 console.log(count1, count2);
 </script>
