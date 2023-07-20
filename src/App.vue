17b2d3608a98896e72861de358ce4a8bcdad4d23
unknown <safaeimobina1381@gmail.com>
2023-07-20 13:07:29 +0330

    modify App using ref in setup

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <template>
-  <div>{{ msg }}</div>
+  <div>
+    <h2>{{ msg }}</h2>
+    <h3 @click="count++">{{ count }}</h3>
+  </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+
 console.log("start");
 const msg = "this is mobina";
+const count=ref(0);
 </script>
